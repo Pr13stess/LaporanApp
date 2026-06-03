0b0ac6375674950d0f5a59843773c3182f095566
Roger Ramopolii <ssora6007@gmail.com>
2026-06-03 16:03:14 +0700

    fix: resolve supabase compatibility issues

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,11 @@ const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (moduleName.includes('@opentelemetry')) {
+  if (
+    moduleName.includes('@opentelemetry') ||
+    moduleName === 'stream' ||
+    moduleName === 'ws'
+  ) {
     return { type: 'empty' };
   }
   return context.resolveRequest(context, moduleName, platform);
